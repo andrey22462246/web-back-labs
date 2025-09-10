f306efd306b132df70a662011aaf058472107c92
Шкуропатов Андрей <andrey.shkuropatov@mail.ru>
2025-09-10 17:47:37 +0700

    Информация об авторе

--- venv/app.py
+++ venv/app.py
@@ -2,10 +2,28 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/web")
 def start():
-    return "<!doctype html>" \
-        "<html>" \
-        "   <body>" \
-        "       <h1>web-сервер на flask</h1>" \
-        "   <body>" \
-        "</html>"
+    return """<!doctype html>
+        <html>
+           <body>
+               <h1>web-сервер на flask</h1>
+                <a href="/author">author</a>
+           </body>
+        </html>"""
+
+@app.route("/author")
+def author():
+    name = "Шкуропатов Андрей Александрович"
+    group = "ФБИ-32"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
