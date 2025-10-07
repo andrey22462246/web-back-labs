281c6c45ed695403d0049f42ef8b708d0a2c2e9c
Шкуропатов Андрей <andrey.shkuropatov@mail.ru>
2025-10-07 10:15:47 +0700

    Имя студента

--- app.py
+++ app.py
@@ -879,4 +879,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Шкуропатов Андрей'
+    return render_template('example.html', name=name)
