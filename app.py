e3710a9685e2612a8fae39ed7e7f3df1c61518f8
Шкуропатов Андрей <andrey.shkuropatov@mail.ru>
2025-10-07 09:44:18 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, redirect
+from flask import Flask, url_for, request, redirect, abort
 import datetime
 
 app = Flask(__name__)
@@ -852,3 +852,13 @@ def a():
 @app.route('/lab2/a/')
 def a2():
     return 'со слешом'
+
+flowers_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id >= len(flowers_list):
+        abort(404)
+    else:
+        return "цветок: " + flowers_list[flower_id]
+    
