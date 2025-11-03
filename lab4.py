17d356efefad74fe693b354b6a734daceb33b884
Шкуропатов Андрей <andrey.shkuropatov@mail.ru>
2025-11-03 17:01:19 +0700

    додел

--- lab4.py
+++ lab4.py
@@ -109,6 +109,13 @@ def tree():
 
     return redirect('/lab4/tree')
 
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'zxc', 'password': '666'},
+    {'login': 'qwerty', 'password': 'qwerty'},
+    {'login': 'lol', 'password': 'kek'},
+]
 @lab4.route('/lab4/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -117,8 +124,9 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == "123":
-        return render_template('/lab4/login.html', login=login, authorized=True)
+    for user in users:  
+        if login == ['login'] and password == ['password']:
+            return render_template('/lab4/login.html', login=login, authorized=True)
 
     error = 'Неверные логин и/или пароль'
     return render_template('lab4/login.html', error=error, authorized=False)
