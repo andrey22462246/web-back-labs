720db5584a35b29c849edae5e7b15f1de19c6700
Андрей Шкуропатов <andrey.shkuropatov@mail.ru>
2025-11-17 19:02:50 +0700

    6756

--- lab5.py
+++ lab5.py
@@ -172,3 +172,15 @@ def login():
     session['username'] = login
     db_close(conn, cur)
     return render_template('lab5/success_login.html', login=login, username=login)
+
+    @lab5.route('/lab5/debug')
+    def debug():
+        conn, cur = db_connect()
+        cur.execute("SELECT COUNT(*) FROM articles")
+        count = cur.fetchone()[0]
+        db_close(conn, cur)
+        return f"Статей в базе: {count}"
+
+    @lab5.route('/lab5/debug_config')
+    def debug_config():
+        return f"DB_TYPE: {current_app.config['DB_TYPE']}"
