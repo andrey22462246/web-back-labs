19dc5050525dec0e16de1e88f87025f91616fdf3
Андрей Шкуропатов <andrey.shkuropatov@mail.ru>
2025-11-17 10:44:25 +0700

    Аутентификация

--- lab5.py
+++ lab5.py
@@ -1,15 +1,13 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
 lab5 = Blueprint('lab5',__name__)
 
 @lab5.route('/lab5/')
 def main():
-    return render_template('lab5/lab5.html')
-
-@lab5.route('/lab5/login')
-def login():
-    return render_template('lab5/login.html')
+    username = session.get('login')
+    return render_template('lab5/lab5.html', login = session.get('login'), username=username)
 
 @lab5.route('/lab5/register', methods = ['GET', 'POST'])
 def register():
@@ -49,3 +47,42 @@ def list_articles():
 @lab5.route('/lab5/create')
 def create_article():
     return render_template('lab5/create.html')
+
+@lab5.route('/lab5/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab5/login.html')
+
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login or password):
+        return render_template('lab5/login.html', error="Заполните поля")
+
+    conn = psycopg2.connect(
+        host='127.0.0.1',
+        database='andrey_shkuropatov_knowledge_base',
+        user='andrey_shkuropatov_knowledge_base',
+        password='123'
+    )
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+
+    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    user = cur.fetchone()
+
+    if not user:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html',
+            error='Логин и/или пароль неверны')
+
+    if user['password'] != password:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html',
+            error='Логин и/или пароль неверны')
+    
+    session['login'] = login
+    cur.close()
+    conn.close()
+    return render_template('lab5/success_login.html', login=login, username = login)
