93a4f5430985528fd38df318e762511f255e8c1c
Шкуропатов Андрей <andrey.shkuropatov@mail.ru>
2025-11-19 18:52:14 +0700

    13page

--- lab6.py
+++ lab6.py
@@ -1,6 +1,30 @@
 from flask import Blueprint, render_template, request, redirect, session
+
 lab6 = Blueprint('lab6',__name__)
+
+offices = []
+for i in range(1,11):
+    offices.append({"number":1, "tenant": ""})
 
 @lab6.route('/lab6/')
 def lab():
     return render_template('lab6/lab6.html')
+
+@lab6.route('/lab6/json-rpc-api/', methods = ['POST'])
+def api():
+    data = request.json
+    id = data['id']
+    if data['method'] == 'info':
+        return{
+            'jsonrpc': '2.0',
+            'result': offices,
+            'id': id
+        }
+    return {
+        'jsonrpc': '2.0',
+        'error': {
+            'code': -32601,
+            'message': 'Method not found'
+        },
+        'id':id
+    }
